c4a2b7a3f1b22adb8cfcc38d008022caba6fd130
Uthpala Herath <ukh0001@mix.wvu.edu>
2020-05-22 03:43:52 -0400

    Added cleanqe.sh

--- cleanqe.sh
+++ cleanqe.sh
@@ -2,4 +2,4 @@
 # This script cleans files generated from Quantum Espresso DFT runs.
 # -Uthpala Herath
 
-rm -rf *.amn *.dat *.kpt *.chk *.mmn *.out *.nnkp *.save *.xml *.agr *.gnu *.eig *.wout UNK*
+rm -rf *.amn *.dat *.kpt *.chk *.mmn *.out *.nnkp *.save *.xml *.agr *.gnu *.eig *.wout UNK* *.wf*
