9aad0c09e70949ae5771ed23de7025d2f1a615f6
Uthpala Herath <uthpala.herath@duke.edu>
2024-04-25 23:09:45 -0400

    Updated absolute path checking in dirdiff.sh

--- dirdiff.sh
+++ dirdiff.sh
@@ -3,8 +3,21 @@
 # from: https://github.com/ZSaberLv0/ZFVimDirDiff
 
 WORK_DIR=$(cd "$(dirname "$0")"; pwd)
-PATH_A=$(pwd)/$1
-PATH_B=$(pwd)/$2
+
+# Check if absolute path is given for PATH_A
+if [[ "$1" = /* ]]; then
+    PATH_A=$1
+else
+    PATH_A=$(pwd)/$1
+fi
+
+# Check if absolute path is given for PATH_B
+if [[ "$2" = /* ]]; then
+    PATH_B=$2
+else
+    PATH_B=$(pwd)/$2
+fi
+
 if test "0" = "1" \
     || test "x-$PATH_A" = "x-" \
     || test "x-$PATH_B" = "x-" \
