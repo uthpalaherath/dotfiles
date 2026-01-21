1ac1388883a6d7018758cb1225bb461d11527a77
Uthpala Herath <uthpala.herath@duke.edu>
2026-01-21 15:49:43 +0000

    Simplify count_group_members.sh

--- slurm/count_group_members.sh
+++ slurm/count_group_members.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
+# Get a count of users in each group
 
-# Get the current user's groups
 for group in $(groups); do
-    # Get supplementary members
     members=$(getent group "$group" | cut -d: -f4 | tr ',' '\n' | grep -v '^$')
     count=$(echo "$members" | wc -l)
-
-    # Print nicely formatted output
     printf "%-30s %5d\n" "$group" "$count"
 done
