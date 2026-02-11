cc30924fc5173b9a4cda74eca20feaa9aabe59c0
Uthpala Herath <uthpala.herath@duke.edu>
2026-02-11 01:53:42 -0500

    Updated gpu_report.sh and partition_gpu_eff_weighted_simple_ordered_mem_csv.sh ncshare emails and slurm-gpu rust binary.

--- slurm/gpu_report.sh
+++ slurm/gpu_report.sh
@@ -27,12 +27,12 @@ mkdir -p "$SLURM_REPORT_DIR"
 
 # dump slurm-report for each of those users in directory
 echo "Generating individual user reports..."
-for user in $(slurm-report -r "$PARTITION" -S "$START_DATE" -E "$END_DATE" --summary --plain | tail -n +5 | awk -F " " '{print $1}'); do
-    slurm-report -r "$PARTITION" -S "$START_DATE" -E "$END_DATE" -u "$user" > "$SLURM_REPORT_DIR/${PARTITION}_${user}_${START_DATE//-/}-${END_DATE//-/}.txt"
+for user in $(slurm-gpu report -r "$PARTITION" -S "$START_DATE" -E "$END_DATE" --summary --plain | tail -n +3 | awk -F " " '{print $1}'); do
+    slurm-gpu report -r "$PARTITION" -S "$START_DATE" -E "$END_DATE" -u "$user" > "$SLURM_REPORT_DIR/${PARTITION}_${user}_${START_DATE//-/}-${END_DATE//-/}.txt"
 done
 
 # Run partition_gpu_eff_weighted_simple_ordered.sh
-./partition_gpu_eff_weighted_simple_ordered_mem_csv.sh -p "$PARTITION" -s "$START_DATE" -e "$END_DATE" -c
+./partition_gpu_eff_weighted_simple_ordered_mem_csv.sh -p "$PARTITION" -s "$START_DATE" -e "$END_DATE"
 
 # Sub-account utilization
 if [ $PARTITION == "h200alloc" ]; then
@@ -40,11 +40,11 @@ if [ $PARTITION == "h200alloc" ]; then
     echo "=== Sub-account utilization ==="
     echo ""
     #sreport cluster AccountUtilizationByQoS Start=$START_DATE End=$END_DATE -T gres/gpu Accounts=slurm-subaccount-testing_h200_r,slurm-subaccount-testing_h200_u format=Account%32,QOS,Used -t Hours | tail -n +4
-    u_sum=$(slurm-report -r h200alloc -A slurm-subaccount-testing_h200_u -S "${START_DATE}" -E "${END_DATE}" --summary --plain \
-            | tail -n +5 \
+    u_sum=$(slurm-gpu report -r h200alloc -A slurm-subaccount-testing_h200_u -S "${START_DATE}" -E "${END_DATE}" --summary --plain \
+            | tail -n +3 \
             | awk '{s+=$3} END {print s}')
-    r_sum=$(slurm-report -r h200alloc -A slurm-subaccount-testing_h200_r -S "${START_DATE}" -E "${END_DATE}" --summary --plain \
-            | tail -n +5 \
+    r_sum=$(slurm-gpu report -r h200alloc -A slurm-subaccount-testing_h200_r -S "${START_DATE}" -E "${END_DATE}" --summary --plain \
+            | tail -n +3 \
             | awk '{s+=$3} END {print s}')
     echo "slurm-subaccount-testing_h200_u: ${u_sum} GPU-hours"
     echo "slurm-subaccount-testing_h200_r: ${r_sum} GPU-hours"
@@ -61,5 +61,9 @@ if [ $PARTITION == "h200alloc" ]; then
     for i in `sacctmgr show assoc where account=slurm-subaccount-testing_h200_u format=user --noheader`; do echo -n $i@duke.edu\;;done
 elif [ $PARTITION == "h200ea" ]; then
     for i in `sacctmgr show assoc where account=h200ea format=user --noheader`; do echo -n $i@duke.edu\;;done
+elif [ $PARTITION == "gpu" ]; then
+    ldapsearch -H ldap://ncshare-com-01.ncshare.org -x -b dc=ncshare,dc=org '(uid=*)' mail \
+  | grep -oE '[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}' \
+  | paste -sd ';' -
 fi
 echo
